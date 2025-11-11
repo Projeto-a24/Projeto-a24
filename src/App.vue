738d491acec1a0cb896e1bb58d33fd3a316af4f7
claratesser <claratesserc@gmail.com>
2025-11-11 15:09:09 -0300

    FEAT: inicializar projeto Vue 3 com Vite, Pinia e Vue Router. #1

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,46 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+  import { ref, onMounted } from 'vue';
+  import api from './plugins/axios';
+
+  const moviesGenres = ref([]);
+  const TVGenres = ref([]);
+
+  onMounted(async () => {
+    let response = await api.get('genre/movie/list?language=pt-BR');
+    moviesGenres.value = response.data.genres;
+    response = await api.get('genre/tv/list?language=pt-BR');
+    TVGenres.value = response.data.genres;
+  });
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <h1>Filmes</h1>
+  <ul class="genre-list">
+    <li v-for="genre in genres" :key="genre.id" class="genre-item">
+      {{ genre.name }}
+    </li>
+  </ul>
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+  height: 3rem;
+  display: flex;
+  background-color: black;
+  color: #fff;
+  font-size: 1.2rem;
+  padding-left: 2rem;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+  column-gap: 2rem;
+  margin-bottom: 0;
+  display: flex;
+  align-items: center;
 }
 
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  text-decoration: none;
+  color: #fff;
 }
 </style>
